369e202aeb7959616a12f7b4c567adf3435a2826
JoaoVitorAR <jvalves1998@outlook.com.br>
2023-08-11 15:32:07 -0300

    Criação CRUD Scaffolding Department

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+        [Key]
+        public int _id { get; set; }
+        public string _name { get; set; }
+    }
+}
