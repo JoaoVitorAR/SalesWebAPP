b13ef28292080136b95d173d65386ac4cbd8a760
JoaoVitorAR <jvalves1998@outlook.com.br>
2023-08-14 10:16:22 -0300

    Adicionando outras entidades e atualizando banco de dados

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace SalesWebMvc.Models
 {
@@ -7,5 +10,24 @@ namespace SalesWebMvc.Models
         [Key]
         public int id { get; set; }
         public string name { get; set; }
+        public ICollection<Seller> sellers { get; set; } = new List<Seller>();
+
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+          sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime intial, DateTime final)
+        {
+            return sellers.Sum(sellers => sellers.TotalSales(intial, final));
+        }
     }
 }
---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public DateTime birthDate { get; set; }
+        public double baseSalary { get; set; }
+        public Department department { get; set; }
+        public ICollection<SalesRecord> salesRec { get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.birthDate = birthDate;
+            this.baseSalary = baseSalary;
+            this.department = department;
+        }
+
+        public void AddSales(SalesRecord sales)
+        {
+            salesRec.Add(sales);
+        }
+
+        public void RemoveSales(SalesRecord sales)
+        {
+            salesRec.Remove(sales);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return salesRec.Where(sales => sales.date >= initial && sales.date <= final)
+                .Sum(sales => sales.amount);
+        }
+    }
+}
